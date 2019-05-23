b97eed79da0f7be56696c7fdd04a174a28396040
Mekhi Jones <mekhi.rafiki@gmail.com>
2019-05-23 05:38:44 -0700

    webscraping improvements

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 import statistics
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 import re
 from bs4 import BeautifulSoup
+import datetime
+from dateutil import parser
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -17,13 +19,17 @@ def readFile(fileName):
     return contents
 
 
-def articleParser(article_page):
+def articleSentimentParser(article_page):
     soup = BeautifulSoup(article_page, features="html.parser")
+    date = None
     text = soup.find_all('article')
+    dateTime = soup.find_all('time')
+    if len(dateTime) > 0:
+        date = dateTime[0]['datetime'][:10]
     if len(text) > 0:
         text = text[0].get_text()
         sentiment = sia.polarity_scores(text)['compound']
-        return sentiment
+        return (sentiment, date)
     else:
         return None
 
@@ -36,47 +42,82 @@ def articleUrlParser(page):
             relevant_articles.append(href) #rhef
     return relevant_articles
 
-def yahooFinanceParser(companySymbol):
-    visitedUrls = set()
+def yahooFinanceParser(companySymbol, companyDict, visitedURLS):
     page = urlopen('https://finance.yahoo.com/quote/' + companySymbol + '/news/').read()
     relevant_articles = articleUrlParser(page)
     sentiments = []
     for articleUrl in relevant_articles:
         url = "https://finance.yahoo.com/" + articleUrl
-        visitedUrls.add(url)
         try:
             article_page = urlopen(url).read()
         except:
             print("Couldn't open one of the links")
             print(articleUrl)
-        sentiment = articleParser(article_page)
-        if sentiment != None:
-            sentiments.append(sentiment)
-            print(articleUrl + " Sentiment " + ": \t" + str(sentiment))
-    sum = 0
-    for rating in sentiments:
-        if rating < SENTIMENT_BOUNDARY:
-            sum += 1
-        elif rating < SENTIMENT_BOUNDARY:
-            sum -= 1
-    overall_sentiment = statistics.mean(sentiments)
-    return (visitedUrls, overall_sentiment, sum)
+        parsedArticleData = articleSentimentParser(article_page)
+        if parsedArticleData != None:
+            sentiment = parsedArticleData[0]
+            date = parsedArticleData[1]
+            dt = parser.parse(date)
+            newVal = (url, sentiment)
+            if dt in companyDict:
+                companyDict[dt].append(newVal)
+            else:
+                companyDict[dt] = [newVal]
+            visitedURLS.add(url)
+    return companyDict
+
+# Two functions below are under development
+# Google finds many ways to block webscrapers
+
+def googleArticleUrlParser(page):
+    soup = BeautifulSoup(page, features="html.parser")
+    relevant_articles = []
+    print(soup.find_all("a"))
+
+def googleFinanceParser(companySymbol, companyDict, visitedURLS):
+    url = 'https://www.google.com/search?q='+ companySymbol + '&tbm=fin'
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    page = urlopen(req).read()
+    googleArticleUrlParser(page)
+
+
+def computeAvgSentiment(companyDict, latestDate):
+    absSentiment = 0
+    avgSentiment = 0
+    validArticleCount = 0
+    for date in companyDict:
+        if date >= latestDate:
+            for articleSentiment in companyDict[date]:
+                if articleSentiment[1] < SENTIMENT_BOUNDARY:
+                    absSentiment += 1
+                elif articleSentiment[1] < SENTIMENT_BOUNDARY:
+                    absSentiment -= 1
+
+                avgSentiment += articleSentiment[1]
+                validArticleCount +=1
+    overall_sentiment = avgSentiment / validArticleCount   #statistics.mean(avgSentiment)
+    return (absSentiment, overall_sentiment)
 
 
 def main():
     companySymbol = "TSLA"
-    afterYahoo = yahooFinanceParser(companySymbol)
-    visitedSet = afterYahoo[0]
-    overall_sentiment = afterYahoo[1]
-    abs = afterYahoo[2]
-    print(abs)
+    companyDict = {}
+    visitedURLS = set()
+    yahooFinanceParser(companySymbol, companyDict, visitedURLS)
+    #googleFinanceParser(companySymbol, companyDict, visitedURLS)
+    latestDate = datetime.datetime(2019, 5, 20)
+    sentiment = computeAvgSentiment(companyDict, latestDate)
+    absSentiment = sentiment[0]
+    overall_sentiment = sentiment[1]
+    print("Based on article sentiment since " + str(latestDate))
+    print("Absolute: " + str(absSentiment) + '\t' + "Overall Sentiment: " + str(overall_sentiment))
+
     if overall_sentiment > 0:
         print("Your requested stock will increase!")
     elif overall_sentiment < 0:
         print("Your requested stock will decrease!")
     else:
         print("Your requested stock will be stable")
-    print(visitedSet)
     #print(relevant_articles)
 
 
